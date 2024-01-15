4fd3758e27725e10084b75f28d057262844d91b7
poula9744 <poula9744@naver.com>
2024-01-15 17:52:41 +0900

    수정

--- src/com/javaex/ex01/Ex02.java
+++ src/com/javaex/ex01/Ex02.java
@@ -7,26 +7,25 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public class Ex02 {
+	
+	public static void main(String[] args) throws IOException {
 
-	public static void main(String[] args) throws IOException{
-		
 		InputStream in = new FileInputStream("C:\\javaStudy\\img.jpg");
 		OutputStream out = new FileOutputStream("C:\\javaStudy\\byBuffImg.jpg");
-		
-		
+
 		byte[] buff = new byte[1024];
-		
+
 		System.out.println("복사 시작");
-		while(true) {
-			int data = in.read(buff); //빈 쟁반을 넣어주면, 채워줌 
-			
-			if(data==1) {
+		while (true) {
+			int data = in.read(buff); // 빈 쟁반을 넣어주면, 채워줌
+
+			if (data == -1) {
 				System.out.println("복사 끝");
 				break;
 			}
 			out.write(buff);
 		}
-		
+
 		in.close();
 		out.close();
 
